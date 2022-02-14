ace0ca1381d281ef3265491c942db8df17c36538
Magnus <magn902m@stud.kea.dk>
2022-02-14 14:37:10 +0100

    Examples in class

--- functions.js
+++ functions.js
@@ -102,3 +102,99 @@
 // function presentPet(firstName, animalType, animalName) {
 //   console.log(`My name is ${firstName}, i have a ${animalType} called ${animalName}`);
 // }
+
+// // ----- examples in class -----
+
+// let number1 = 2;
+// let theOtherNumber = 35;
+// let result;
+
+// // this function is not dynamic
+// // function multiply() {
+// //   result = number1 * theOtherNumber;
+// // }
+
+// // the parameters make the function mere dynamic
+// function multiply(num1, num2) {
+//   result = num1 * num2;
+// }
+
+// // this is how to change a para to a other value
+// function multiply(anOldBoot, num2) {
+//   let anotherBoot = 2 * anOldBoot;
+//   result = anotherBoot * num2;
+// }
+
+// // this i mini fydt code, there is hard to read by human
+// function _m(a, b) {
+//   let a = 2 * a;
+//   c = a * b;
+// }
+
+// // num1 doesn´t exsis here, only in the function
+// console.log(num1);
+
+// multiply(number1, theOtherNumber);
+// // multiply(23, 1000);
+
+// console.log("Result: ", result);
+// console.log("Result: ", c);
+
+// // -----
+
+// let aNumber = 3;
+// let theThirdNumber;
+
+// function addingNumber(aNumber, bNumber) {
+//   let theThirdNumber = 5;
+//   console.log(aNumber + bNumber + theThirdNumber);
+// }
+
+// addingNumber(2, 4);
+
+// // -----
+
+// sayHello("Peter");
+// console.log(aNumber);
+
+// function sayHello(firstName) {
+//   console.log(`Hello ${firstName}`);
+// }
+
+// let sayWhatEver = function (firstName) {
+//   console.log(`What ever ${firstName}`);
+// };
+
+// let aNumber = 34;
+
+// let sayHoney = (firstName) => {
+//   console.log(`Honey ${firstName}`);
+// };
+
+// sayWhatEver("Peter");
+// sayHoney("Peter");
+
+// window.addEventListener("DOMContentLoaded", start);
+// // window.addEventListener("DOMContentLoaded", () => {});
+
+// function start(event) {
+//   console.log("I am here");
+// }
+
+// let myName = "Peter";
+
+// sayHello(myName);
+
+// function sayHello(firstName) {
+//   console.log(`Hello ${firstName}`);
+// }
+
+// // -----
+
+// let myName = "Peter";
+
+// sayHello(myName, "some string", 23);
+
+// function sayHello(firstName, b, c) {
+//   console.log(`Hello ${firstName} and ${b} and ${c}`);
+// }

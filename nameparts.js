05757bcfe09067c8151f2d488252cb3df44047e7
Magnus <magn902m@stud.kea.dk>
2022-02-14 17:44:22 +0100

    Exercise - FullName function in a func

--- nameparts.js
+++ nameparts.js
@@ -54,22 +54,44 @@
 
 // const fullName = `Magnus "Maskinen" Nielsen`;
 
+// function fullName(lastName, firstName, middleName) {
+//   if (middleName === undefined) {
+//     console.log(`Fullname: ${firstName} ${lastName}`);
+//   } else {
+//     console.log(`Fullname: ${firstName} ${middleName} ${lastName}`);
+//   }
+// }
+
+// fullName("Potter", "Harry", "James");
+// // // should console.log "Harry James Potter"
+
+// fullName("Potter", "Harry");
+// // // should console.log "Harry Potter"
+
+// fullName("Harry", "Potter");
+// // // should console.log "Potter Harry"
+
+// fullName("Potter", "Harry", "James", "Pottypotpot");
+// // // should console.log "Harry James Potter"
+
+// -----
+
+const aName = "Albus Percival Wulfric Brian Dumbledore";
+
+function getNameParts(fullName) {
+  const firstName = fullName.substring(fullName.indexOf(0), fullName.indexOf(" "));
+  const middleName = fullName.substring(fullName.indexOf(" ") + 1, fullName.lastIndexOf(" "));
+  const lastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
+  console.log({ firstName, middleName, lastName });
+  fullName(lastName, firstName, middleName);
+}
+
 function fullName(lastName, firstName, middleName) {
-  if (middleName === undefined) {
-    console.log(`Fullname: ${lastName} ${firstName}`);
+  if (middleName) {
+    console.log(`Fullname: ${firstName} ${middleName} ${lastName}`);
   } else {
-    console.log(`Fullname: ${lastName} ${firstName} ${middleName}`);
+    console.log(`Fullname: ${firstName} ${lastName}`);
   }
 }
 
-fullName("Potter", "Harry", "James");
-// // should console.log "Harry James Potter"
-
-fullName("Potter", "Harry");
-// // should console.log "Harry Potter"
-
-fullName("Harry", "Potter");
-// // should console.log "Potter Harry"
-
-fullName("Potter", "Harry", "James", "Pottypotpot");
-// // should console.log "Harry James Potter"
+getNameParts(aName);

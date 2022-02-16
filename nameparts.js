188ab577963437af655d17bf78fad5e4987f0855
Magnus <magn902m@stud.kea.dk>
2022-02-16 09:04:26 +0100

    Exercise - getNameParts( fullName ) done

--- nameparts.js
+++ nameparts.js
@@ -139,3 +139,36 @@
 // }
 
 // console.log(nameParts);
+
+// --- Exercise - getNameParts( fullName )
+
+const aName = "ALbus PerCIval wulfric BRIaN dumbLEdOre";
+const nameParts = getNameParts(aName);
+
+function capitalize(str) {
+  let capitalizedStr = str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+  return `${capitalizedStr}`;
+}
+
+function getNameParts(inputName) {
+  const firstName = capitalize(inputName.substring(inputName.indexOf(0), inputName.indexOf(" ")));
+  const middleName = capitalizationMiddleName(
+    inputName.substring(inputName.indexOf(" ") + 1, inputName.lastIndexOf(" "))
+  );
+  const lastName = capitalize(inputName.substring(inputName.lastIndexOf(" ") + 1));
+  //console.log({ firstName, middleName, lastName });
+  return { lastName, firstName, middleName };
+}
+
+function capitalizationMiddleName(middleName) {
+  let middleNameArr = middleName.split("");
+  middleNameArr.forEach((elm, index, arr) => {
+    if (elm === " ") {
+      arr[index + 1] = arr[index + 1].toUpperCase() + arr[index + 1].substring(1).toLowerCase();
+    }
+  });
+  let result = middleNameArr.join("");
+  return result;
+}
+
+console.log(nameParts);
